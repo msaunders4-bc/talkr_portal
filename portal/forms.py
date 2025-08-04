5aca4ac6fb38e674ecdbd336043033d58bfb6c07
marc <msaunders4@hotmail.co.uk>
2025-08-04 11:37:15 +0100

    remove contract_end_date

--- portal/forms.py
+++ portal/forms.py
@@ -10,8 +10,19 @@ class EmployeeForm(forms.ModelForm):
             'embg', 'mk_id', 'start_date', 'net_salary'
         ]
         widgets = {
-            'start_date': forms.DateInput(attrs={'type': 'date'}),
-            'net_salary': forms.NumberInput(attrs={'step': '0.01'}),
+            'start_date': forms.DateInput(
+                attrs={
+                    'type': 'date',
+                    'class': 'form-control'
+                },
+                format='%Y-%m-%d'
+            ),
+            'net_salary': forms.NumberInput(
+                attrs={
+                    'step': '0.01',
+                    'class': 'form-control'
+                }
+            ),
         }
     
     def __init__(self, *args, **kwargs):
@@ -20,3 +31,6 @@ class EmployeeForm(forms.ModelForm):
         for field_name, field in self.fields.items():
             if field_name != 'client':
                 field.required = True
+            # Add CSS classes to all fields
+            if 'class' not in field.widget.attrs:
+                field.widget.attrs['class'] = 'form-control'
