684cc6a76d7c60fc052f79deb62d10ecae1bccf4
marc <msaunders4@hotmail.co.uk>
2025-08-05 13:19:35 +0100

    make the database_url consistent between env.py and heroku

--- config/settings.py
+++ config/settings.py
@@ -84,10 +84,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# DATABASES
 
-# Database configuration
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 if DATABASE_URL:
