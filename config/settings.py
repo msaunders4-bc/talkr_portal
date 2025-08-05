c51f9afaa616d94649bcbf9fbe170d939378ffa1
marc <msaunders4@hotmail.co.uk>
2025-08-05 10:18:08 +0100

    update settings.database to fix heroku

--- config/settings.py
+++ config/settings.py
@@ -94,8 +94,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #}
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+        )
 }
 
 if 'test' in sys.argv:
